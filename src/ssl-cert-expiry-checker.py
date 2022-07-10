6584681da9701079f020138ddcb64056b7c9df08
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-07-10 15:16:50 +0000

    integrations: Add Zulip notifications.

--- src/ssl-cert-expiry-checker.py
+++ src/ssl-cert-expiry-checker.py
@@ -3,7 +3,7 @@ from utils.helpers import process_config_file
 import socket
 import ssl
 from datetime import datetime
-from utils.notifications import send_completion_discord_message, send_error_discord_message, send_expire_discord_message
+from utils.notifications import send_completion_discord_message, send_error_discord_message, send_expire_discord_message, send_completion_zulip_message, send_error_zulip_message, send_expire_zulip_message
 
 def check_ssl_cert_for_expiry(domain, conf_options) -> int:
     """
@@ -22,6 +22,8 @@ def check_ssl_cert_for_expiry(domain, conf_options) -> int:
             print(f"The domain {domain} has a self-signed-cert which isn't supported.")
             if "Discord" in conf_options['APP']['NOTIFICATIONS']:
                 send_error_discord_message(f"The domain {domain} has a self-signed-cert which isn't supported.", conf_options)
+            if "ZulipAPI" in conf_options['APP']['NOTIFICATIONS']:
+                send_error_zulip_message(f"The domain {domain} has a self-signed-cert which isn't supported.", conf_options)
             return -2
         elif "certificate has expired" in e.verify_message:
             return -1
@@ -29,6 +31,8 @@ def check_ssl_cert_for_expiry(domain, conf_options) -> int:
         print(f"The domain {domain} has no website for us to check against.")
         if "Discord" in conf_options['APP']['NOTIFICATIONS']:
                 send_error_discord_message(f"The domain {domain} has no website for us to check against.", conf_options)
+        if "ZulipAPI" in conf_options['APP']['NOTIFICATIONS']:
+                send_error_zulip_message(f"The domain {domain} has no website for us to check against.", conf_options)
         return -2
         
 
@@ -45,17 +49,25 @@ def main() -> None:
             print(f"The domain {domain} has {days} days left.")
             if "Discord" in conf_options['APP']['NOTIFICATIONS']:
                 send_expire_discord_message(domain, f"Expires in {days} days.", conf_options)
+            if "ZulipAPI" in conf_options['APP']['NOTIFICATIONS']:
+                send_expire_zulip_message(domain, f"is set to expires in {days} days.", conf_options)
         elif days == 0:
             print(f"The domain {domain} expires today.")
             if "Discord" in conf_options['APP']['NOTIFICATIONS']:
-                send_expire_discord_message(domain, f"Expires today", conf_options)
+                send_expire_discord_message(domain, f"Expires today.", conf_options)
+            if "ZulipAPI" in conf_options['APP']['NOTIFICATIONS']:
+                send_expire_zulip_message(domain, f"expires today.", conf_options)
         elif days == -1:
             print(f"The domain {domain} has expired.")
             if "Discord" in conf_options['APP']['NOTIFICATIONS']:
                 send_expire_discord_message(domain, f"Expired", conf_options)
+            if "ZulipAPI" in conf_options['APP']['NOTIFICATIONS']:
+                send_expire_zulip_message(domain, f"is expired.", conf_options)
     
     if 'Discord' in conf_options['APP']['NOTIFICATIONS']:
         send_completion_discord_message(conf_options)
+    if 'ZulipAPI' in conf_options['APP']['NOTIFICATIONS']:
+        send_completion_zulip_message(conf_options)
 
 if __name__ == "__main__":
     main()
--- src/utils/notifications.py
+++ src/utils/notifications.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import zulip
 from datetime import datetime, timezone
 
 def send_expire_discord_message(domain, status, config_options) -> None:
@@ -46,3 +47,42 @@ def send_error_discord_message(error, config_options) -> None:
     payload = json.dumps(payload, indent=4)
 
     response = requests.request("POST", config_options['APP']['DISCORD_WEBHOOK_ERROR'], headers=headers, data=payload)
+
+def send_completion_zulip_message(config_options):
+    # Pass the path to your zuliprc file here.
+    client = zulip.Client(config_file=config_options['APP']['ZULIP_BOT_FILE'])
+
+    # Send a stream message
+    request = {
+        "type": "stream",
+        "to": config_options['APP']['ZULIP_STREAM'],
+        "topic": "SSL Certificate Expiry Check Complete",
+        "content": "All domain's SSL Certificates have been successfully checked for expiry." 
+    }
+    result = client.send_message(request)
+
+def send_expire_zulip_message(domain, status, config_options):
+    # Pass the path to your zuliprc file here.
+    client = zulip.Client(config_file=config_options['APP']['ZULIP_BOT_FILE'])
+
+    # Send a stream message
+    request = {
+        "type": "stream",
+        "to": config_options['APP']['ZULIP_STREAM'],
+        "topic": domain,
+        "content": f"The SSL Certificate for the domain {domain} {status}"
+    }
+    result = client.send_message(request)
+
+def send_error_zulip_message(error, config_options):
+    # Pass the path to your zuliprc file here.
+    client = zulip.Client(config_file=config_options['APP']['ZULIP_BOT_FILE'])
+
+    # Send a stream message
+    request = {
+        "type": "stream",
+        "to": config_options['APP']['ZULIP_ERROR_STREAM'],
+        "topic": "SSL Certificate Expiry Check Error",
+        "content": error 
+    }
+    result = client.send_message(request)
